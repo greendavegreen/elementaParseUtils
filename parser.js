2e1d560b9e93eeaf0b5a828b49eebe9e715ba9a7
Dave Green <greendavegreen@gmail.com>
2016-06-18 22:54:54 -0400

    first cut of parser lib

---
+++ parser.js
@@ -0,0 +1,136 @@
+function ArticleDoc (content) {
+    this.dom = require('xmldom').DOMParser;
+    this.xp = require('xpath');
+    this.doc = new this.dom().parseFromString(content);
+}
+
+
+
+ArticleDoc.prototype.node_array_text = function(path) {
+    var nodes = this.xp.select(path + '/text()', this.doc);
+    var result = nodes.map(String);
+    return result;
+};
+
+ArticleDoc.prototype.node_text = function(path) {
+    var query = path + '//text()';
+    var nodes = this.xp.select(query, this.doc);
+    return nodes.join('');
+};
+
+ArticleDoc.prototype.node_raw = function(path) {
+    reps = [{ from:'<bold>', to: '<b>'},
+            { from:'</bold>', to: '</b>'},
+            { from:'<italic>', to: '<em>'},
+            { from:'</italic>', to: '</em>'}];
+
+
+    var query = path + '/node()';
+    var raw = this.xp.select(query, this.doc).join('');
+
+    var arrayLength = reps.length;
+    for (var i = 0; i < arrayLength; i++) {
+        raw = raw.replace(reps[i].from, reps[i].to);
+    }
+    return raw;
+};
+
+ArticleDoc.prototype.build_aff = function(node) {
+
+    var dept = this.xp.select1('institution[@content-type="dept"]/text()', node);
+    var inst = this.xp.select1('institution[not(@content-type="dept")]/text()', node);
+    var city = this.xp.select1('addr-line[@content-type="city"]/text()', node);
+    var prov = this.xp.select1('addr-line[@content-type="state"]/text()', node);
+    var country = this.xp.select1('country/text()', node);
+
+    var result = {};
+    if (typeof dept !== 'undefined') result.dept = dept.data;
+    if (typeof inst !== 'undefined') result.institute = inst.data;
+    if (typeof city !== 'undefined') result.city = city.data;
+    if (typeof prov !== 'undefined') result.province = prov.data;
+    if (typeof country !== 'undefined') result.country = country.data;
+    return result;
+
+};
+
+ArticleDoc.prototype.add_affiliations = function(auth_node, author) {
+    var aff_nodes = this.xp.select('xref[@ref-type="aff"]/@rid', auth_node);
+
+    aff_list = [];
+    for (var i=0;i<aff_nodes.length; i++) {
+        var node = aff_nodes[i];
+        var item = this.xp.select1("//article-meta/aff[@id='" + node.value + "']", this.doc);
+        aff_list.push(this.build_aff(item));
+    }
+
+    author.affs = aff_list;
+};
+
+ArticleDoc.prototype.add_mail = function(auth_node, author) {
+    var corr = this.xp.select1('xref[@ref-type="corresp"]/@rid', auth_node);
+    if (typeof corr !== 'undefined') {
+        var address = this.xp.select1("//author-notes/corresp[@id='" + corr.value + "']/email/text()", corr)
+        if (typeof address !== 'undefined') {
+            author.email = address.data;
+        }
+    }
+};
+
+ArticleDoc.prototype.author_array = function() {
+    authors = [];
+    var authorsQuery = "//contrib-group/contrib[@contrib-type='author' or @contrib-type='corresponding-author']";
+    var nodes = this.xp.select(authorsQuery, this.doc);
+    for (var i=0;i<nodes.length; i++) {
+        var node = nodes[i];
+        var name = {
+            "surname": this.xp.select1('name/surname/text()', node).data,
+            "givenName": this.xp.select1('name/given-names/text()', node).data
+        };
+        this.add_affiliations(node, name);
+        this.add_mail(node, name);
+        authors.push(name);
+    }
+    return authors;
+};
+
+ArticleDoc.prototype.newArticle = function() {
+    var article = {"journal_tag": 'elementa', "state": 'parsed', "loc": 'staged'};
+    var xp = this.xp;
+    var doc = this.doc;
+
+    article.authors = this.author_array();
+
+    article.title = this.node_text('/article/front/article-meta/title-group/article-title');
+    article.publisher = this.node_text('/article/front/journal-meta/journal-id[@journal-id-type="publisher-id"]');
+    article.doi = this.node_text('/article/front/article-meta/article-id[@pub-id-type="doi"]');
+    article.journal_abbr = this.node_text('/article/front/journal-meta/journal-title-group/abbrev-journal-title');
+    article.volume = this.node_text('/article/front/article-meta/volume[@content-type="volume"]');
+    article.eloc = this.node_text('/article/front/article-meta/elocation-id');
+    article.issn = this.node_text('/article/front/journal-meta/issn[@pub-type="epub"]');
+
+    article.anum = parseInt(article.eloc, 10);
+    article.issue = '1';
+
+    var title =  this.node_text('/article/front/journal-meta/journal-title-group/journal-title');
+    var sub =  this.node_text('/article/front/journal-meta/journal-title-group/journal-subtitle');
+    article.journal = title + ': ' + sub;
+
+    article.article_type = this.node_text('/article/front/article-meta/article-categories/subj-group[@subj-group-type="article-type"]/subject');
+
+    /* must do larger work for these */
+    article.html_title = this.node_raw('/article/front/article-meta/title-group/article-title');
+
+    var date_node = this.xp.select('/article/front/article-meta/pub-date[@pub-type="epub"]/@iso-8601-date', this.doc);
+
+    article.pub_date =  new Date(date_node[0].value).getTime();
+
+
+    article.domains= this.node_array_text('/article/front/article-meta/article-categories/subj-group[@subj-group-type="domain"]/subject');
+
+    article.copyright = 'STUB_lib';
+
+    return article;
+
+};
+
+module.exports = ArticleDoc;
---
+++ test/parseTest.js
@@ -0,0 +1,56 @@
+var assert = require('chai').assert;
+var fs = require('fs')
+var ap = require ('../parser.js');
+
+
+
+describe('parserTest', function() {
+    var article;
+
+    before(function() {
+        var data = fs.readFileSync('test/elementa.999998.xml', 'utf8');
+        var parser = new ap(data);
+        article = parser.newArticle();
+    });
+
+    describe('#newArticle()', function () {
+        it('should find HTML tags in html_title', function () {
+            assert.equal('What\'s the <b>worst</b> that could happen?', article.html_title);
+        });
+        it('should find 2 domains', function () {
+            assert.deepEqual([ 'Ecology', 'Ocean Science' ], article.domains);
+        });
+        it('should find 2 domains', function () {
+            assert.equal(new Date("2084-9-28").getTime(), article.pub_date);
+        });
+        it('should find 1 author', function () {
+            assert.deepEqual([ { givenName: "Sølfrid S.",
+                                 surname: "Hj&oslash;llo",
+                                 "email": "jack.handy@BTT.org",
+                                 affs: [
+                                     
+                                     {
+                                         "city": "Fairbanks",
+                                         "country": "United States",
+                                         "institute": "Big Think Tank",
+                                         "province": "Alaska"
+                                     },{
+                                         "city": "White Plains",
+                                         "country": "United States",
+                                         "dept": "Complaints Department",
+                                         "institute": "IBM",
+                                         "province": "New York",
+                                     }
+                                        ]
+                               }
+                              ], article.authors);
+        });
+        it('should find title', function () {
+            assert.equal('What\'s the worst that could happen?', article.title);
+        });
+    });
+
+});
+
+
+
